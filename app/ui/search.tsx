09d0d2eee6168fc7f917aa6c081413ca7969c23f
EBregains <ebregains@gmail.com>
2024-01-08 17:04:54 -0300

    Added a react server action to create invoice

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -22,8 +22,9 @@ export default function Search({placeholder}: {placeholder : string}) {
     else
       params.delete('query');
 
-    replace(`${pathname}?${params.toString()}`);
+    replace(`${pathname}?${params.toString()}`); // Dynamicly changes de URL without refreshing the whole page 
   },400)
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
