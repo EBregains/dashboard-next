c5cec8d2c2400dfd939ac3d763f2ca73b4068e25
EBregains <ebregains@gmail.com>
2024-01-09 11:28:21 -0300

    add label errors for Update Invoice Inputs

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -70,7 +70,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
   redirect('/dashboard/invoices');
 }
 
-export async function updateInvoice(id:string, formData: FormData) {
+export async function updateInvoice(id:string, prevState: State, formData: FormData) {
 
      // Validate form using Zod
      const validatedFields = UpdateInvoice.safeParse({
