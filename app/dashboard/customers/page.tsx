1b2aaf5518dd332d952e7e5570b169e4eb6ccd2e
EBregains <ebregains@gmail.com>
2024-01-09 16:05:20 -0300

    add Customers Page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,13 +1,32 @@
+import { fetchCustomers, fetchFilteredCustomers } from '@/app/lib/data';
+import Table from '@/app/ui/customers/table';
+import { InvoicesTableSkeleton, TableRowSkeleton } from '@/app/ui/skeletons';
 import { Metadata } from 'next';
+import { Suspense } from 'react';
  
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function CustomerPage() {
+export default async function Page( {
+  searchParams,
+} : {
+  searchParams?: {
+    query?: string;
+    page?: number;
+  };
+}) {
+
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+
+  const customers = await fetchFilteredCustomers(query);
+
   return (
-    <>
-      <p> Customer page </p>
-    </>
+    <div className="w-full">  
+      <Suspense key={query + currentPage} fallback={<TableRowSkeleton />}>
+        <Table customers={customers} />
+      </Suspense>
+    </div>
   )
 }
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,13 +9,8 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
-import { unstable_noStore as noStore } from 'next/cache';
 
 export async function fetchRevenue() {
-  // Add noStore() here prevent the response from being cached.
-  // This is equivalent to in fetch(..., {cache: 'no-store'}).
-  noStore()
-
   try {
 
     console.log('Fetching revenue data...');
@@ -33,7 +28,6 @@ export async function fetchRevenue() {
 }
 
 export async function fetchLatestInvoices() {
-  noStore()
   try {
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
@@ -54,7 +48,6 @@ export async function fetchLatestInvoices() {
 }
 
 export async function fetchCardData() {
-  noStore()
   try {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
@@ -94,7 +87,6 @@ export async function fetchFilteredInvoices(
   query: string,
   currentPage: number,
 ) {
-  noStore()
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
   try {
@@ -127,7 +119,6 @@ export async function fetchFilteredInvoices(
 }
 
 export async function fetchInvoicesPages(query: string) {
-  noStore()
   try {
     const count = await sql`SELECT COUNT(*)
     FROM invoices
@@ -149,7 +140,6 @@ export async function fetchInvoicesPages(query: string) {
 }
 
 export async function fetchInvoiceById(id: string) {
-  noStore()
   try {
     const data = await sql<InvoiceForm>`
       SELECT
@@ -175,7 +165,6 @@ export async function fetchInvoiceById(id: string) {
 }
 
 export async function fetchCustomers() {
-  noStore()
   try {
     const data = await sql<CustomerField>`
       SELECT
@@ -194,7 +183,6 @@ export async function fetchCustomers() {
 }
 
 export async function fetchFilteredCustomers(query: string) {
-  noStore()
   try {
     const data = await sql<CustomersTableType>`
 		SELECT
@@ -228,7 +216,6 @@ export async function fetchFilteredCustomers(query: string) {
 }
 
 export async function getUser(email: string) {
-  noStore()
   try {
     const user = await sql`SELECT * FROM users WHERE email=${email}`;
     return user.rows[0] as User;
